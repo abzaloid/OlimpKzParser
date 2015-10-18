8302c9addb62067fb0cd49457b7fbaa7d4d4ada4
Abzal Serekov <abzal.serekov@gmail.com>
2015-10-19 00:42:53 +0600

    divided into parts

--- doall.py
+++ doall.py
@@ -3,6 +3,10 @@ import urllib2
 from bs4 import BeautifulSoup
 import re
 
+date = []
+win = []
+lost = []
+
 ########################
 #	login
 ########################
@@ -31,13 +35,15 @@ r = requests.post("https://www.olimpkz.com/index.php", data=keys)
 
 r = requests.get("https://www.olimpkz.com/index.php?page=history", cookies=r.cookies)
 soup = BeautifulSoup(r.text, 'html5lib')
+
+# parse data using CSS selectors
+
 win_data = soup.select('html > body > table > tbody > tr > td.central_td > center > center > table > tbody > tr > td > b > i')
 lost_data = soup.select('html > body > table > tbody > tr > td.central_td > center > center > table > tbody > tr > td > b')
 date_data = soup.select('html > body > table > tbody > tr > td.central_td > center > center > table > tbody > tr > td.txtmed')
 
-date = []
-win = []
-lost = []
+
+# populate data
 
 for i in range(len(date_data)):
 	date.append(str(date_data[i]))
@@ -48,25 +54,45 @@ for i in range(len(win_data)):
 for i in range(len(win_data)):
 	lost.append(str(lost_data[2 * i + 1]))
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# prettify data
 ww = []
 for w in date:
 	p = re.findall(r'[0-9]{2}.[0-9]{2}.[0-9]{4} [0-9]{2}:[0-9]{2}',w)
 	ww.append(p[0])
-
 date = ww
-
 ww = []
 for w in win:
 	p = re.findall(r'\d+',w)
 	ww.append(p[0])
-
 win = ww
-
 ww = []
 for w in lost:
 	p = re.findall(r'\d+',w)
 	ww.append(p[0])
-
 lost = ww
 
 
