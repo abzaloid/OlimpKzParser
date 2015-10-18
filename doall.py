651e96874bd5d29ee129d62d87930a7da69b077d
Abzal Serekov <abzal.serekov@gmail.com>
2015-10-19 00:40:49 +0600

    autologin is done

---
+++ doall.py
@@ -0,0 +1,75 @@
+import requests
+import urllib2
+from bs4 import BeautifulSoup
+import re
+
+########################
+#	login
+########################
+
+r = requests.get("https://www.olimpkz.com")
+
+login = "667310"
+passw = "Qwerty123321"
+
+txt = r.text
+psid = re.findall(r"(?i)<input type=hidden name='psid' value='([^>]+)'>",txt)
+asid = re.findall(r"(?i)<input type=hidden name='asid' value='([^>]+)'>",txt)
+
+keys = {
+	'login': login,
+	'passw': passw,	
+	'psid': psid,
+	'asid': asid,
+}
+
+r = requests.post("https://www.olimpkz.com/index.php", data=keys)
+
+##########################
+#	history page redirect
+##########################
+
+r = requests.get("https://www.olimpkz.com/index.php?page=history", cookies=r.cookies)
+soup = BeautifulSoup(r.text, 'html5lib')
+win_data = soup.select('html > body > table > tbody > tr > td.central_td > center > center > table > tbody > tr > td > b > i')
+lost_data = soup.select('html > body > table > tbody > tr > td.central_td > center > center > table > tbody > tr > td > b')
+date_data = soup.select('html > body > table > tbody > tr > td.central_td > center > center > table > tbody > tr > td.txtmed')
+
+date = []
+win = []
+lost = []
+
+for i in range(len(date_data)):
+	date.append(str(date_data[i]))
+
+for i in range(len(win_data)):
+	win.append(str(win_data[i]))
+
+for i in range(len(win_data)):
+	lost.append(str(lost_data[2 * i + 1]))
+
+ww = []
+for w in date:
+	p = re.findall(r'[0-9]{2}.[0-9]{2}.[0-9]{4} [0-9]{2}:[0-9]{2}',w)
+	ww.append(p[0])
+
+date = ww
+
+ww = []
+for w in win:
+	p = re.findall(r'\d+',w)
+	ww.append(p[0])
+
+win = ww
+
+ww = []
+for w in lost:
+	p = re.findall(r'\d+',w)
+	ww.append(p[0])
+
+lost = ww
+
+
+print win
+print lost
+print date
