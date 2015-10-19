9d4c8ff8cea3139d018f56ea09f830f65cce964a
Abzal Serekov <abzal.serekov@gmail.com>
2015-10-19 17:55:01 +0600

    now it is able to crawl olimpkz with session stored

--- doall.py
+++ doall.py
@@ -3,99 +3,97 @@ import urllib2
 from bs4 import BeautifulSoup
 import re
 
-date = []
-win = []
-lost = []
 
 ########################
 #	login
 ########################
 
-r = requests.get("https://www.olimpkz.com")
+login = ""
+passw = ""
 
-login = "667310"
-passw = "Qwerty123321"
+def run(_login, _passw):
+	login = _login
+	passw = _passw
 
-txt = r.text
-psid = re.findall(r"(?i)<input type=hidden name='psid' value='([^>]+)'>",txt)
-asid = re.findall(r"(?i)<input type=hidden name='asid' value='([^>]+)'>",txt)
+	print login, passw
 
-keys = {
-	'login': login,
-	'passw': passw,	
-	'psid': psid,
-	'asid': asid,
-}
-
-r = requests.post("https://www.olimpkz.com/index.php", data=keys)
-
-##########################
-#	history page redirect
-##########################
-
-r = requests.get("https://www.olimpkz.com/index.php?page=history", cookies=r.cookies)
-soup = BeautifulSoup(r.text, 'html5lib')
-
-# parse data using CSS selectors
-
-win_data = soup.select('html > body > table > tbody > tr > td.central_td > center > center > table > tbody > tr > td > b > i')
-lost_data = soup.select('html > body > table > tbody > tr > td.central_td > center > center > table > tbody > tr > td > b')
-date_data = soup.select('html > body > table > tbody > tr > td.central_td > center > center > table > tbody > tr > td.txtmed')
+	date = []
+	win = []
+	lost = []
 
 
-# populate data
+	r = requests.get("https://www.olimpkz.com")
 
-for i in range(len(date_data)):
-	date.append(str(date_data[i]))
+	txt = r.text
+	psid = re.findall(r"(?i)<input type=hidden name='psid' value='([^>]+)'>",txt)
+	asid = re.findall(r"(?i)<input type=hidden name='asid' value='([^>]+)'>",txt)
 
-for i in range(len(win_data)):
-	win.append(str(win_data[i]))
+	keys = {
+		'login': login,
+		'passw': passw,	
+		'psid': psid,
+		'asid': asid,
+	}
 
-for i in range(len(win_data)):
-	lost.append(str(lost_data[2 * i + 1]))
-
-
-
-
-
-
-
+	s = requests.session()
+	r = s.post("https://www.olimpkz.com/index.php", data=keys)
 
 
+	##########################
+	#	history page redirect
+	##########################
+
+	r = s.get("https://www.olimpkz.com/index.php?page=history")
+	soup = BeautifulSoup(r.text, 'html5lib')
+	
+	
+	if (len(BeautifulSoup(r.text, 'html5lib').select('form')) != 2):
+		return ([], [], [])
+
+	page_size = len(soup.select('select')[0])
+
+	for page in range(1, page_size + 1):
+		payload = {
+			'offset': str(page),
+			'page': 'history',
+		}
+
+		r = s.post("https://www.olimpkz.com/index.php", data=payload)
+		soup = BeautifulSoup(r.text, 'html5lib')
+		# parse data using CSS selectors
+
+		win_data = soup.select('html > body > table > tbody > tr > td.central_td > center > center > table > tbody > tr > td > b > i')
+		lost_data = soup.select('html > body > table > tbody > tr > td.central_td > center > center > table > tbody > tr > td > b')
+		date_data = soup.select('html > body > table > tbody > tr > td.central_td > center > center > table > tbody > tr > td.txtmed')
 
 
+		# populate data
+
+		for i in range(len(date_data)):
+			date.append(str(date_data[i]))
+
+		for i in range(len(win_data)):
+			win.append(str(win_data[i]))
+
+		for i in range(len(win_data)):
+			lost.append(str(lost_data[2 * i + 1]))
 
 
+	# prettify data
+	ww = []
+	for w in date:
+		p = re.findall(r'[0-9]{2}.[0-9]{2}.[0-9]{4} [0-9]{2}:[0-9]{2}',w)
+		ww.append(p[0])
+	date = ww
+	ww = []
+	for w in win:
+		p = re.findall(r'\d+',w)
+		ww.append(p[0])
+	win = ww
+	ww = []
+	for w in lost:
+		p = re.findall(r'\d+',w)
+		ww.append(p[0])
+	lost = ww
 
-
-
-
-
-
-
-
-
-
-
-
-# prettify data
-ww = []
-for w in date:
-	p = re.findall(r'[0-9]{2}.[0-9]{2}.[0-9]{4} [0-9]{2}:[0-9]{2}',w)
-	ww.append(p[0])
-date = ww
-ww = []
-for w in win:
-	p = re.findall(r'\d+',w)
-	ww.append(p[0])
-win = ww
-ww = []
-for w in lost:
-	p = re.findall(r'\d+',w)
-	ww.append(p[0])
-lost = ww
-
-
-print win
-print lost
-print date
+	return (date, win, lost)
