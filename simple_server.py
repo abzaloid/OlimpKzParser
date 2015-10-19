bf811fb6e9c72228c0c53993136f6589b695fc1c
Abzal Serekov <abzal.serekov@gmail.com>
2015-10-19 17:55:47 +0600

    simple server on python with only GET method

---
+++ simple_server.py
@@ -0,0 +1,53 @@
+import sys
+import BaseHTTPServer
+from BaseHTTPServer import BaseHTTPRequestHandler
+import urlparse
+from SimpleHTTPServer import SimpleHTTPRequestHandler
+import doall
+import json
+
+class GetHandler(BaseHTTPRequestHandler):
+    
+    def do_GET(self):
+        parsed_path = urlparse.urlparse(self.path)
+        response = ""
+        date = []
+        win = []
+        lost = []
+        message = parsed_path.query
+        if len(message) < 20:
+        	response = -1
+        else:
+	        login = message[6:12]
+	        passw = message[19:]
+	        (date, win, lost) = doall.run(login, passw)
+
+        self.send_response(200)
+        self.end_headers()
+        if response == "":
+        	j = json.dumps({"date": date, 
+        				"win": win,
+        				"lost": lost,})
+        	response = json.loads(j)
+        self.wfile.write(response)
+        return
+
+# HandlerClass = SimpleHTTPRequestHandler
+ServerClass  = BaseHTTPServer.HTTPServer
+Protocol     = "HTTP/1.0"
+
+if sys.argv[1:]:
+    port = int(sys.argv[1])
+else:
+    port = 8765
+server_address = ('127.0.0.1', port)
+
+# HandlerClass.protocol_version = Protocol
+httpd = ServerClass(server_address, GetHandler)
+
+sa = httpd.socket.getsockname()
+print "Serving HTTP on", sa[0], "port", sa[1], "..."
+
+
+
+httpd.serve_forever()
